4c3bdee044c74802680d73cc58e40224acf61d24
Tyler Smart <tsmart@redhat.com>
2011-08-12 11:17:43 -0400

    First commit

---
+++ egel/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Egel::Application.config.secret_token = '6b081d9cba8618c5233e4755d055889881b9784f3edf174033ad9f1868cbd7ef0466f3568cb58bbdbf0578f7fe0c6d20485bce3c2bada5c45802d257d6b4331e'
